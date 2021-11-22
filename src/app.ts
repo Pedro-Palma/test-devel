84e9d0eddc1fc690271c5d780e10012762b146cf
Pedro Palma <peter.gokussj@gmail.com>
2021-11-22 06:20:49 -0600

    complete endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import user from './routes/user'
 import poll from './routes/poll'
-
+import type from './routes/type'
 const app = express();
 
 app.use(express.json());
@@ -10,6 +10,7 @@ app.disable('etag');
 
 app.use('/api',user);
 app.use('/api',poll);
+app.use('/api',type)
 
 
 export default app;
